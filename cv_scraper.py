8b5a6f1e0da51c1e9550e5b75b28eb4896379e7e
Unknown <uoshvis@gmail.com>
2018-01-04 15:41:08 +0200

    code revision

--- cv_scraper.py
+++ cv_scraper.py
@@ -35,7 +35,21 @@ def find_pages_range(soup):
     return max_page
 
 
+def _format_result(result):
+    result.replace('\n', '')
+    result.replace('\t', '')
+    result.replace(u'\xa0', u' ')
+    result.rstrip()
+
+    return result
+
+
 def scrape_ad_page(url):
+    """Scrapes a single add page
+    :param url: url of single ad page
+    :type url: str
+    :returns: dict of single add data
+    """
     responsibilities = None
     qualifications = None
     benefits = None
@@ -44,58 +58,57 @@ def scrape_ad_page(url):
     contact_email = None
 
     soup = get_soup(url=url)
-    results = soup.find_all('h2')
-    for result in results:
-        if result:
-            if result.get_text() == 'Darbo pobūdis:':
-                result = result.find_next_sibling()
+    headings_2 = soup.find_all('h2')
+
+    job_text = soup.find("div", class_="jobTxtRp")
+
+    if job_text:
+        job_text = job_text.get_text()
+
+    for heading_2 in headings_2:
+        if heading_2:
+            heading_2_text = heading_2.get_text()
+            if heading_2_text == 'Darbo pobūdis:':
+                result = heading_2.find_next_sibling()
                 responsibilities = ''
                 for rez in result:
                     if rez.string:
                         responsibilities += rez.string + ' '
-                responsibilities.replace('\n', '')
-                responsibilities.replace('\t', '')
-                responsibilities.replace(u'\xa0', u' ')
-                responsibilities.rstrip()
+                responsibilities = _format_result(responsibilities)
 
-            if result.get_text() == 'Reikalavimai:':
-                result = result.find_next_sibling()
+            if heading_2_text == 'Reikalavimai:':
+                result = heading_2.find_next_sibling()
                 qualifications = ''
                 for rez in result:
                     if rez.string:
                         qualifications += rez.string + ' '
-                qualifications.replace('\n', '')
-                qualifications.replace('\t', '')
-                qualifications.replace(u'\xa0', u' ')
-                qualifications.rstrip()
+                qualifications = _format_result(qualifications)
 
-            if 'siūlo' in result.get_text():
-                result = result.find_next_sibling()
+            if 'siūlo' in heading_2_text:
+                result = heading_2.find_next_sibling()
                 benefits = ''
                 for rez in result:
                     if rez.string:
                         benefits += rez.string + ' '
-                benefits.replace('\n', '')
-                benefits.replace('\t', '')
-                benefits.replace(u'\xa0', u' ')
-                benefits.rstrip()
+                benefits = _format_result(benefits)
 
-    results = soup.find('table', id='jobTxtRTable')
-    results = results.find_all('tr')
-    for result in results:
-        td = result.find_all('td')
-        for td1 in td:
-            if 'iki' in td1.get_text():
-                if td1.find_next_sibling():
-                    jobad_expiration = td1.find_next_sibling().get_text()
+    job_table = soup.find('table', id='jobTxtRTable')
+    job_table = job_table.find_all('tr')
+
+    for row in job_table:
+        td = row.find_all('td')
+        for data in td:
+            if 'iki' in data.get_text():
+                if data.find_next_sibling():
+                    jobad_expiration = data.find_next_sibling().get_text()
                     jobad_expiration = jobad_expiration.replace('.', '-')
                     jobad_expiration = datetime.strptime(jobad_expiration, '%Y-%m-%d')
-            if td1.get_text() in ['Contact person:' , 'Kontaktinis asmuo:']:
-                if td1.find_next_sibling():
-                    contact_person = td1.find_next_sibling().get_text()
-            if td1.get_text() in ['El. paštas:', 'E-mail:']:
-                if td1.find_next_sibling():
-                    contact_email_line = str(td1.find_next_sibling())
+            if data.get_text() in ['Contact person:' , 'Kontaktinis asmuo:']:
+                if data.find_next_sibling():
+                    contact_person = data.find_next_sibling().get_text()
+            if data.get_text() in ['El. paštas:', 'E-mail:']:
+                if data.find_next_sibling():
+                    contact_email_line = str(data.find_next_sibling())
                     match = re.search(r'[\w\.\-\_]+@[\w\.\-\_]+\.\w+', contact_email_line)
                     contact_email = match.group(0)
 
@@ -105,8 +118,10 @@ def scrape_ad_page(url):
         'benefits': benefits,
         'jobad_expiration': jobad_expiration,
         'contact_person': contact_person,
-        'contact_email': contact_email
+        'contact_email': contact_email,
+        'job_text': job_text
     }
+
     return return_data
 
 
@@ -119,22 +134,28 @@ def scrape_list_page(soup):
     for ad in list_ads:
         # find position
         position = ad.find('a', itemprop='title').get_text()
+
         # find company
         company = ad.find('a', itemprop='hiringOrganization')
         company = company.get_text()
         company = company.replace('"', '')
         company = company.replace('\'', '')
-        # find data_posted
+
+        # find date posted
         date_posted = ad.find('meta', itemprop='datePosted')['content']
         date_posted = datetime.strptime(date_posted, '%Y-%m-%d')
 
         # find views
         views = ad.find('span', class_='visited').get_text()
         views = int(views.replace('.', ''))
+
         # find place
         place = ad.find('meta', itemprop='jobLocation')['content']
+
         # find ad url
         url_sri = ad.find('a', itemprop='title')
+
+        # scrape single ad
         if url_sri:
             url_sri = prefix + url_sri['href']
             ad_id = int(re.search('\d-(\d+)', url_sri).groups()[0])
@@ -171,7 +192,7 @@ def main():
     page_range = find_pages_range(soup_main)
     print('page_range {}'.format(page_range))
     while init_page <= page_range:
-        print('Scannnig page --> {}'.format(init_page))
+        print('Scannnig page: {}'.format(init_page))
         soup = get_soup(url=None, init_page=init_page)
         scraped_data = scrape_list_page(soup)
 
