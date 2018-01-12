0f680403538efa27d10c2f467296afc7213c7893
Unknown <uoshvis@gmail.com>
2018-01-12 18:48:30 +0200

    add list retrieve search destroy

--- main.py
+++ main.py
@@ -8,6 +8,44 @@ import cv_scraper as cv
 from write_to_mongodb import write_positions_db
 
 
+def search_positions(city, keyword):
+    '''
+    function used by REST API
+    '''
+    SITES_AVAILABLE = [cvbankas]
+
+    for site in SITES_AVAILABLE:
+        print('Scraping from: {site}'.format(site=site.__name__))
+        page_number = 1
+        get_soup = getattr(site, 'get_soup')
+        soup_main = get_soup(
+            city=city,
+            keyword=keyword,
+            page_number=page_number
+        )
+        find_pages_range = getattr(site, 'find_pages_range')
+        page_range = find_pages_range(soup_main)
+        print('Page range of {site} is {range}.'.format(
+            site=site.__name__,
+            range=page_range)
+        )
+
+        while page_number <= page_range:
+            print('Scannnig page {}'.format(page_number))
+            soup = get_soup(
+                city=city,
+                keyword=keyword,
+                page_number=page_number
+            )
+            scraped_ads = getattr(site, 'scrape_list_page')(soup)
+            print('Scraped ads: ', scraped_ads)
+            write_positions_db(scraped_ads, 'gogo')
+            page_number += 1
+            sleep(randint(1, 5))
+
+    print('Job Done. From main.')
+
+
 def main():
     SITES_AVAILABLE = [cvbankas]
 
--- positions/views.py
+++ positions/views.py
@@ -1,15 +1,47 @@
-from rest_framework_mongoengine import viewsets
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import list_route
+
 from positions.serializers import PositionsSerializer
 from positions.models import Positions
+from main import search_positions
 
 
-class PositionsViewSet(viewsets.ModelViewSet):
+class PositionsViewSet(viewsets.ViewSet):
     '''
-    Contains information about inputs/outputs of a single program
-    that may be used in Universe workflows.
+    Returs positions from db
+    or searches for new positions
     '''
-    lookup_field = 'id'
-    serializer_class = PositionsSerializer
+    def retrieve(self, request, pk=None):
+        queryset = Positions.objects.filter(id=pk).first()
+        serializer = PositionsSerializer(queryset)
+        data = serializer.data
+        return Response(data)
 
-    def get_queryset(self):
-        return Positions.objects.all()
+    def list(self, request):
+        queryset = Positions.objects.all()
+        serializer = PositionsSerializer(queryset, many=True)
+        data = serializer.data
+
+        return Response(data)
+
+    @list_route(methods=['GET'])
+    def search(self, request):
+        if request.query_params:
+            city = request.query_params.get('city', '')
+            keyword = request.query_params.get('keyword', '')
+            search_positions(city=city, keyword=keyword)
+        queryset = Positions.objects.all()
+        serializer = PositionsSerializer(queryset, many=True)
+        data = serializer.data
+
+        return Response(data)
+
+    def destroy(self, request, pk=None):
+        queryset = Positions.objects.filter(id=pk).first()
+        if not queryset:
+            raise Exception('object not found in database')
+        queryset.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
